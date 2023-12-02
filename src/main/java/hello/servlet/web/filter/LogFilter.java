f54562ffb16e5e0e4adac363b7a9ef876c9ccedc
ejejc <ejejc@naver.com>
2023-12-02 23:59:12 +0900

    서블릿 필터-요청 로그2

--- src/main/java/hello/servlet/web/filter/LogFilter.java
+++ src/main/java/hello/servlet/web/filter/LogFilter.java
@@ -1,9 +1,11 @@
 package hello.servlet.web.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 public class LogFilter implements Filter {
@@ -16,6 +18,20 @@ public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response); // 다음 필터 호출, 다음 필터가 존재하지 않으면 서블릿 호출
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
     }
 
     @Override
